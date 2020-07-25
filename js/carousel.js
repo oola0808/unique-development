6a715da576f88a53f1a59a7b3fc3457fa9f312ed
Kristin Baggett <msbaggett0808@gmail.com>
2020-07-25 17:57:13 -0400

    new nav menu

--- js/carousel.js
+++ js/carousel.js
@@ -46,3 +46,13 @@ $(document).ready(function(){
   modalClose.addEventListener('click', function() {
     modal.style.display = "none";
   });
+
+ /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
